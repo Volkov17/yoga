eb60d8a50f58254c13873637da4e146d880f0555
Alexander <alexander.andr.volkov@gmail.com>
2020-04-26 20:02:43 +0300

    add modal window

--- js/script.js
+++ js/script.js
@@ -85,4 +85,24 @@ window.addEventListener('DOMContentLoaded', function(){
         }
 
         setClock('timer' , deadline);
+
+
+        // modal window 
+
+        let more = document.querySelector('.more'),
+            overlay = document.querySelector('.overlay'),
+            close = document.querySelector('.popup-close');
+
+        more.addEventListener('click' , function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden'; 
+        });
+
+        close.addEventListener('click' , function(){
+            overlay.style.add = 'none';
+            more.classList.remove('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+
 });
