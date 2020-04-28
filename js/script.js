1423a5c69fa06baf0f562f5295c8f467d797e9be
Alexander <alexander.andr.volkov@gmail.com>
2020-04-28 19:19:31 +0300

    calc

--- js/script.js
+++ js/script.js
@@ -136,10 +136,8 @@ window.addEventListener('DOMContentLoaded', () =>{
                     //request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
                     request.addEventListener('readystatechange', function (){
                         if (request.readyState < 4 ){
-                    
                             resolve();
                         } else if ( request.readyState === 4 && request.status == 200){
-                            statusMessage.innerHTML = message.success;
                             resolve();
                         } else { reject();}
                     });
@@ -161,10 +159,7 @@ window.addEventListener('DOMContentLoaded', () =>{
                               .then( ()=> statusMessage.innerHTML = message.loading)
                               .then( ()=> statusMessage.innerHTML = message.success)
                               .catch( ()=> statusMessage.innerHTML = message.failure);
-
- 
-        
-    
+   
             for( let i = 0 ; i < input.length; i++){
                 input[i].value = '';
             }
@@ -198,4 +193,100 @@ window.addEventListener('DOMContentLoaded', () =>{
                 input[i].value = '';
             }
         });
+
+        //slider
+
+        let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) {
+        //     slides[i].style.display = 'none';
+        // }
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n); 
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
+        //calc
+
+        let persons = document.querySelectorAll('.counter-block-input')[0],
+            restDays = document.querySelectorAll('.counter-block-input')[1],
+            place = document.getElementById('select'),
+            totalValue = document.getElementById('total'),
+            personsSum = 0,
+            daysSum = 0,
+            total = 0;
+        
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function(){
+            personsSum = +this.value;
+            total = (daysSum+personsSum)*4000;
+            if ( restDays.value == "" || personsSum.value == ""){
+                totalValue.innerHTML=0;
+            } else {
+                totalValue.innerHTML=total;
+            }
+        });
+
+        restDays.addEventListener('change', function(){
+            daysSum = +this.value;
+            total = (daysSum+personsSum)*4000;
+            if ( personsSum.value == "" || restDays.value == ""){
+                totalValue.innerHTML=0;
+            } else {
+                totalValue.innerHTML=total;
+            }
+        });
+
+        place.addEventListener('change',function(){
+            if( restDays.value == "" || persons.value == ""){
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+        });
+       
+
+
 });
